55a6a435b0b724716a39ecdc5ed2d1915eb19f87
Kai <kaismith3027@gmail.com>
2014-03-08 15:10:08 -0500

    Added projectiles

---
+++ Assets/Scripts/ClusterBomb.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClusterBomb : MonoBehaviour {
+
+	private bool active; //Can hurt tanks when active
+	public GameObject bullet;
+	public int numberOfBullets;
+
+	// Use this for initialization
+	void Start () {
+		active = true;
+		gameObject.GetComponent<TrailRenderer>().material.SetColor("_Color", Color.yellow);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		Vector3 dir;
+		if (collision.collider.tag == "Ground") {
+			dir = new Vector3 (0, 1, 0);
+		} else {
+			ContactPoint contact = collision.contacts[0];
+			dir = contact.normal.normalized;
+		}
+		for (int shot = 0; shot < numberOfBullets; shot++) {
+			Vector3 randdir = new Vector3(dir.x + Random.Range(-100, 100)/1000f, dir.y + Random.Range(-100, 100)/100f, dir.z + Random.Range(-100, 100)/100f).normalized;
+			GameObject b = Instantiate (bullet, gameObject.transform.position + dir*0.5f, Quaternion.identity) as GameObject;
+			b.rigidbody.AddForce (dir * 300);
+		}
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tank : MonoBehaviour {
+
+	private int health;
+
+	// Use this for initialization
+	void Start () {
+		health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Hurt (int amount){
+		health -= amount;
+		if (health <= 0)
+			Die ();
+	}
+
+	private void Die(){
+		Destroy (gameObject);
+	}
+}
